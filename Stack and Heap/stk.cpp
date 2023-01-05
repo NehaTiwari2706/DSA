79d38e4384c6e7694a87d1bf4297f29c6613ccfb
Neha <103049412+NehaTiwari2706@users.noreply.github.com>
2023-01-06 02:30:08 +0530

    Add files via upload

---
+++ Stack and Heap/stk.cpp
@@ -0,0 +1,58 @@
+//IMPLEMENTATION OF STACK USING LINKED LIST
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+
+node* top=NULL;
+
+void push(int val){
+    node* n=new node(val);
+    n->data=val;
+    n->next=top;
+    top=n;
+}
+
+void display(){
+
+    node* temp=top;
+    if(top==NULL){
+        cout<<"Underflow Condition"<<endl;
+    }
+
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+void Top(){
+    if(top==NULL){
+        cout<<"Stack is empty"<<endl;
+    }
+    cout<<top->data<<endl;
+}
+
+int main(){
+
+    push(2);
+    push(3);
+    push(10);
+    display();
+    cout<<endl;
+    Top();
+    // pop();
+    // top();
+    // display();
+
+    return 0;
+}
