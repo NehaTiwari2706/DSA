e8ec7d25b6baf053248f1ad38debe5b800576e28
Neha <103049412+NehaTiwari2706@users.noreply.github.com>
2023-01-06 02:14:33 +0530

    Create strings.cpp

---
+++ String/strings.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string str;
+
+    cin>>str;
+    cout<<str<<endl;
+
+    string str1(5,'n');
+    cout<<str1<<endl;
+
+    string str2="ApniKaksha";
+    cout<<str2;
+
+    return 0;
+}
