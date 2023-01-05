d765342e58b3cd950dbbebaaf2d2b9f2de2ac445
Neha <103049412+NehaTiwari2706@users.noreply.github.com>
2023-01-06 02:18:58 +0530

    Create recursion.cpp

---
+++ Recursion/recursion.cpp
@@ -0,0 +1,21 @@
+//PROGRAM TO ADD ALL NUMBERS TILL N
+#include"bits/stdc++.h"
+using namespace std;
+
+int sum(int n){
+
+    if(n==0){
+        return 0;
+    }
+
+    return n+sum(n-1);
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    cout<<sum(n)<<endl;
+    return 0;
+}
